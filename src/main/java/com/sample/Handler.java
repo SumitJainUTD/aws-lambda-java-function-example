2bb24a178d73a33bad260d2940313fc7d685b3a1
SumitJainUTD <sumi12june@gmail.com>
2022-01-23 20:18:27 -0600

    initial commit

---
+++ src/main/java/com/sample/Handler.java
@@ -0,0 +1,15 @@
+package com.sample;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+public class Handler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent, Context context) {
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+        response.setBody("this is a sample response from a lambda function");
+        return response;
+    }
+}
